f1af0c9c2a2560d1c09d5a96ed2b3049519887da
alibama <adp6j@virginia.edu>
2023-02-10 11:43:22 -0500

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import ldap
+
+def search_user(username):
+    try:
+        l = ldap.initialize("ldap://pitchfork.itc.virginia.edu")
+        l.protocol_version = 3
+        l.simple_bind_s()
+
+        search_filter = f"(&(objectClass=person)(cn={username}))"
+        result = l.search_s("dc=example,dc=com", ldap.SCOPE_SUBTREE, search_filter)
+
+        if not result:
+            return "User not found"
+
+        return result
+    except ldap.LDAPError as e:
+        return f"LDAP error: {e}"
+
+st.title("LDAP User Lookup")
+
+username = st.text_input("Enter username:")
+
+if username:
+    result = search_user(username)
+    st.write("Result:", result)
